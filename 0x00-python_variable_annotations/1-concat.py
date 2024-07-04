59ff6e78b48b9a2e9ff60ee9e2b0502470dc18eb
Shayne999 <shaynejuniorn@gmail.com>
2024-07-04 12:57:15 +0200

    Halfway Fixes

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,4 +5,5 @@ Defines a function that returns a concatenated string
 
 
 def concat(str1: str, str2: str) -> str:
+    """Returns a concatenated string"""
     return str1 + str2
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -7,5 +7,5 @@ Defines a function that returns the floor of the float
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     return math.floor(n)
