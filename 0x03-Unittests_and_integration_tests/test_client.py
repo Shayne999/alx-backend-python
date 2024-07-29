aa8d035ca70938311d6bfad5a7e340eaa633a864
Shayne999 <shaynejuniorn@gmail.com>
2024-07-29 17:26:44 +0200

    4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,14 +14,9 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",)
         ("abc",)
     ])
-    @patch('client.get_json', return_value={})
-    def test_org(self, org_name, mock_get_json):
-        """Test org"""
-        client = GithubOrgClient(org_name)
-        org_data = client.org
-
-        mock_get_json.assert_called_once_with(
-            GithubOrgClient.ORG_URL.format(org=org_name)
-        )
-
-        self.assertEqual(org_data, {})
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """Test that GithubOrgClient.org returns the correct value"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
