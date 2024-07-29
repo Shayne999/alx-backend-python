11481c158fc26f5c902b6b87d3172005c39d5897
Shayne999 <shaynejuniorn@gmail.com>
2024-07-29 18:02:16 +0200

    7

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -64,6 +64,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
     ])
-    def test_has_license(self, repo, license_key, expected_license):
+    def test_has_license(self, repo, license_key, expected):
         """Test has license"""
-        self.assertEqual(GithubOrgClient.has_license(repo, license_key), expected_license)
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
