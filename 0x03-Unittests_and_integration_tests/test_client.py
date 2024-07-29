9b861c40e2f00a1bcc8d75b4d22ccc051876672a
Shayne999 <shaynejuniorn@gmail.com>
2024-07-29 17:35:40 +0200

    5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,9 +14,23 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",)
         ("abc",)
     ])
-    @patch('client.get_json')
-    def test_org(self, input, mock):
-        """Test that GithubOrgClient.org returns the correct value"""
-        test_class = GithubOrgClient(input)
-        test_class.org()
-        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+    @patch('client.get_json', return_value={})
+    def test_org(self, org_name, mock_get_json):
+        """Test org"""
+        client = GithubOrgClient(org_name)
+        org_data = client.org
+
+        mock_get_json.assert_called_once_with(
+            GithubOrgClient.ORG_URL.format(org=org_name)
+        )
+
+        self.assertEqual(org_data, {})
+
+    @patch('client.GithubOrgClient.org', new_callable=unittest.mock.PropertyMock)
+    def test_public_repos(self, mock_org):
+        """Test public repos"""
+        mock_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_org.return_value = mock_payload
+
+        client = GithubOrgClient("google")
+        self.assertEqual(client._publick_repos_url, mock_payload["repos_url"])
